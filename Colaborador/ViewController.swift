70254b49e815ed32f9eda1b9e7fb1fc1b2c14326
Gustavo <gustavo_santanaramirez@elcorteingles.com>
2016-03-16 19:17:59 +0000

    Initial Commit

---
+++ Colaborador/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Colaborador
+//
+//  Created by Gustavo Adolfo Santana Ramírez on 16/3/16.
+//  Copyright © 2016 Gustavo Adolfo Santana Ramírez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
